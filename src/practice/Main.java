08abde205fba5d1a1ad6406bee89f6de45e38741
vevdokimenko <evdokimenko@hotmail.com>
2021-10-30 12:33:25 +0300

    Practice

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -1,2 +1,43 @@
-package practice;public class Main {
+package practice;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Product product1 = new Product("Computer", 5000, 3);
+        Product product2 = new Product("Notebook", 8000, 5);
+        Product product3 = new Product("Monitor", 1250, 4);
+
+        Product[] productsArray = {product1, product2, product3};
+
+        List<Product> productsList = new ArrayList<>();
+        productsList.add(product1);
+        productsList.add(product2);
+        productsList.add(product3);
+
+        Arrays.sort(productsArray, new Comparator<Product>() {
+            @Override
+            public int compare(Product o1, Product o2) {
+                return o1.productPrice - o2.productPrice;
+            }
+        });
+
+        for (Product item : productsArray) {
+            System.out.println(item);
+        }
+
+        System.out.println("========================================");
+
+        Collections.sort(productsList, new Comparator<Product>() {
+            @Override
+            public int compare(Product o1, Product o2) {
+                return o1.productNumber - o2.productNumber;
+            }
+        });
+
+        Iterator<Product> iter = productsList.iterator();
+        while (iter.hasNext()) {
+            System.out.println(iter.next());
+        }
+    }
 }
--- src/practice/Product.java
+++ src/practice/Product.java
@@ -1,2 +1,22 @@
-package practice;public class Product {
+package practice;
+
+public class Product {
+    public String productName;
+    public int productPrice;
+    public int productNumber;
+
+    public Product(String productName, int productPrice, int productNumber) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productNumber = productNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productName='" + productName + '\'' +
+                ", productPrice=" + productPrice +
+                ", productNumber=" + productNumber +
+                '}';
+    }
 }
